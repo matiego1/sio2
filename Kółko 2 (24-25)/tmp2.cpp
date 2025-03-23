79fd792bd2b4fb975bfb7c6685cb076a4a8f7cba
Matiego <84844909+matiego1@users.noreply.github.com>
2025-03-23 18:04:16 +0100

    Add files via upload

---
+++ Kółko 2 (24-25)/tmp2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	// https://codeforces.com/contest/221/submission/312097790
+	cout << "Zrobione";
+
+	return 0;
+}
